e0c0e8761248c6c1d32050a89f8d572cf3a54930
Aslam Abbas <xachu4u@gmail.com>
2018-06-30 17:29:11 +0530

    new template

--- js/main.js
+++ js/main.js
@@ -1,270 +1,269 @@
-/*---------------------------------------------------------------------------------
-/*
-/* Main JS
-/*
------------------------------------------------------------------------------------*/  
+/* =================================
+------------------------------------
+	Labs - Design Studio
+	Version: 1.0
+ ------------------------------------ 
+ ====================================*/
+
+'use strict';
+
+/*------------------
+	Preloder
+--------------------*/
+function loader() {
+	$(window).on('load', function() { 
+		$(".loader").fadeOut(); 
+		$("#preloder").delay(400).fadeOut("slow");
+	});
+}
+
+
+
+/*------------------
+	Navigation
+--------------------*/
+function responsive() {
+	// Responsive 
+	$('.responsive').on('click', function(event) {
+		$('.menu-list').slideToggle(400);
+		event.preventDefault();
+	});
+}
+
+
+
+/*------------------
+	Hero Section
+--------------------*/
+function heroSection() {
+	//Slide item bg image.
+	$('.hero-item').each(function() {
+		var image = $(this).data('bg');
+		$(this).css({
+			'background-image'  : 'url(' + image + ')',
+			'background-size'   : 'cover',
+			'background-repeat' : 'no-repeat',
+			'background-position': 'center bottom'
+		});
+	});
+	//slider auto height 
+	var iit = setInterval(slide_item, 1);
+
+	function slide_item() {
+		var bh = $('body').height();
+		$('.hero-item').height(bh);
+	}
+	slide_item();
+
+	var time = 7;
+	var $progressBar,
+		$bar, 
+		$elem, 
+		isPause, 
+		tick,
+		percentTime;
+
+	// Init the carousel
+	$('#hero-slider').owlCarousel({
+		loop: true,
+		nav: true,
+		items: 1,
+		autoHeight:true,
+		animateOut: 'fadeOut',
+		animateIn: 'fadeIn',
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		onInitialized: progressBar,
+		onTranslated: moved,
+		onDrag: pauseOnDragging
+	});
+
+	// Init progressBar where elem is $("#owl-demo")
+	function progressBar(){    
+		// build progress bar elements
+		buildProgressBar();
+
+		// start counting
+		start();
+	}
+
+	// create div#progressBar and div#bar then prepend to $("#owl-demo")
+	function buildProgressBar(){
+		$progressBar = $("<div>",{
+			id:"progressBar"
+		});
+		$bar = $("<div>",{
+			id:"bar"
+		});
+		$progressBar.append($bar).prependTo($("#hero-slider"));
+	}
+
+	function start() {
+		// reset timer
+		percentTime = 0;
+		isPause = false;
+		// run interval every 0.01 second
+		tick = setInterval(interval, 10);
+	};
+
+	function interval() {
+		if(isPause === false){
+			percentTime += 1 / time;
+
+			$bar.css({
+				width: percentTime+"%"
+			});
+
+			// if percentTime is equal or greater than 100
+			if(percentTime >= 100){
+				// slide to next item 
+				$("#hero-slider").trigger("next.owl.carousel");
+				percentTime = 0; // give the carousel at least the animation time ;)
+			}
+		}
+	}
+
+	// pause while dragging 
+	function pauseOnDragging(){
+		isPause = true;
+	}
+
+	// moved callback
+	function moved(){
+		// clear interval
+		clearTimeout(tick);
+		// start again
+		start();
+	}
+
+}
+
+
+
+/*------------------
+	Video Popup
+--------------------*/
+function videoPopup() {
+	$('.video-popup').magnificPopup({
+		type: 'iframe',
+		autoplay : true
+	});
+}
+
+
+
+/*------------------
+	Testimonial
+--------------------*/
+function testimonial() {
+	// testimonial Carousel 
+	$('#testimonial-slide').owlCarousel({
+		loop:true,
+		autoplay:true,
+		margin:30,
+		nav:false,
+		dots: true,
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:2
+			},
+			800:{
+				items:2
+			},
+			1000:{
+				items:2
+			}
+		}
+	});
+}
+
+
+
+/*------------------
+	Progress bar
+--------------------*/
+function progressbar() {
+
+	$('.progress-bar-style').each(function() {
+		var progress = $(this).data("progress");
+		var prog_width = progress + '%';
+		if (progress <= 100) {
+			$(this).append('<div class="bar-inner" style="width:' + prog_width + '"><span>' + prog_width + '</span></div>');
+		}
+		else {
+			$(this).append('<div class="bar-inner" style="width:100%"><span>' + prog_width + '</span></div>');
+		}
+	});
+}
+
+
+
+/*------------------
+	Accordions
+--------------------*/
+function accordions() {
+	$('.panel').on('click', function (e) {
+		$('.panel').removeClass('active');
+		var $this = $(this);
+		if (!$this.hasClass('active')) {
+			$this.addClass('active');
+		}
+		e.preventDefault();
+	});
+}
+
+
+
+/*------------------
+	Progress Circle
+--------------------*/
+function progressCircle() {
+	//Set progress circle 1
+	$("#progress1").circleProgress({
+		value: 0.75,
+		size: 175,
+		thickness: 5,
+		fill: "#2be6ab",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+	//Set progress circle 2
+	$("#progress2").circleProgress({
+		value: 0.83,
+		size: 175,
+		thickness: 5,
+		fill: "#2be6ab",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+	//Set progress circle 3
+	$("#progress3").circleProgress({
+		value: 0.25,
+		size: 175,
+		thickness: 5,
+		fill: "#2be6ab",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+	//Set progress circle 4
+	$("#progress4").circleProgress({
+		value: 0.95,
+		size: 175,
+		thickness: 5,
+		fill: "#2be6ab",
+		emptyFill: "rgba(0, 0, 0, 0)"
+	});
+
+}
 
 (function($) {
-
-	"use strict";
-
-	/*---------------------------------------------------- */
-	/* Preloader
-	------------------------------------------------------ */ 
-   $(window).load(function() {
-
-      // will first fade out the loading animation 
-    	$("#loader").fadeOut("slow", function(){
-
-        // will fade out the whole DIV that covers the website.
-        $("#preloader").delay(300).fadeOut("slow");
-
-      });       
-
-  	})
-
-
-  	/*----------------------------------------------------*/
-  	/* Flexslider
-  	/*----------------------------------------------------*/
-  	$(window).load(function() {
-
-	  	$('#hero-slider').flexslider({
-	   	namespace: "flex-",
-	      controlsContainer: ".hero-container",
-	      animation: 'fade',
-	      controlNav: true,
-	      directionNav: false,
-	      smoothHeight: true,
-	      slideshowSpeed: 7000,
-	      animationSpeed: 600,
-	      randomize: false,
-	      before: function(slider){
-			   $(slider).find(".animated").each(function(){
-			   	$(this).removeAttr("class");
-			  	});			  	
-			},
-			start: function(slider){
-			   $(slider).find(".flex-active-slide")
-			           	.find("h1").addClass("animated fadeInDown show")
-			           	.next().addClass("animated fadeInUp show");
-			           		
-			   $(window).trigger('resize');		  			 
-			},
-			after: function(slider){
-			 	$(slider).find(".flex-active-slide")
-			           	.find("h1").addClass("animated fadeInDown show")
-			           	.next().addClass("animated fadeInUp show");			  
-			}
-	   });
-
-	   $('#testimonial-slider').flexslider({
-	   	namespace: "flex-",
-	      controlsContainer: "",
-	      animation: 'slide',
-	      controlNav: true,
-	      directionNav: false,
-	      smoothHeight: true,
-	      slideshowSpeed: 7000,
-	      animationSpeed: 600,
-	      randomize: false,
-	   });
-
-   });
-
-
-   /*----------------------------------------------------*/
-	/* Adjust Primary Navigation Background Opacity
-	------------------------------------------------------*/
-   $(window).on('scroll', function() {
-
-		var h = $('header').height();
-		var y = $(window).scrollTop();
-      var header = $('#main-header');
-
-	   if ((y > h + 30 ) && ($(window).outerWidth() > 768 ) ) {
-	      header.addClass('opaque');	      
-	   }
-      else {
-         if (y < h + 30) {
-            header.removeClass('opaque');
-         }
-         else {
-            header.addClass('opaque');
-         }
-      }
-
-	});
-
-
-   /*----------------------------------------------------*/
-  	/* Highlight the current section in the navigation bar
-  	------------------------------------------------------*/
-	var sections = $("section"),
-	navigation_links = $("#nav-wrap a");	
-
-	sections.waypoint( {
-
-       handler: function(direction) {
-
-		   var active_section;
-
-			active_section = $('section#' + this.element.id);
-
-			if (direction === "up") active_section = active_section.prev();
-
-			var active_link = $('#nav-wrap a[href="#' + active_section.attr("id") + '"]');			
-
-         navigation_links.parent().removeClass("current");
-			active_link.parent().addClass("current");
-
-		}, 
-
-		offset: '25%'
-
-	});
-
-
-   /*----------------------------------------------------*/
-  	/* FitText Settings
-  	------------------------------------------------------ */
-  	setTimeout(function() {
-
-   	$('#hero-slider h1').fitText(1, { minFontSize: '30px', maxFontSize: '49px' });
-
-  	}, 100);
-
-
-  	/*-----------------------------------------------------*/
-  	/* Mobile Menu
-   ------------------------------------------------------ */  
-   var menu_icon = $("<span class='menu-icon'>Menu</span>");
-  	var toggle_button = $("<a>", {                         
-                        id: "toggle-btn", 
-                        html : "",
-                        title: "Menu",
-                        href : "#" } 
-                        );
-  	var nav_wrap = $('nav#nav-wrap')
-  	var nav = $("ul#nav");  
-   
-   /* if JS is enabled, remove the two a.mobile-btns 
-  	and dynamically prepend a.toggle-btn to #nav-wrap */
-  	nav_wrap.find('a.mobile-btn').remove(); 
-  	toggle_button.append(menu_icon); 
-   nav_wrap.prepend(toggle_button); 
-
-  	toggle_button.on("click", function(e) {
-   	e.preventDefault();
-    	nav.slideToggle("fast");     
-  	});
-
-  	if (toggle_button.is(':visible')) nav.addClass('mobile');
-  	$(window).resize(function() {
-   	if (toggle_button.is(':visible')) nav.addClass('mobile');
-    	else nav.removeClass('mobile');
-  	});
-
-  	$('ul#nav li a').on("click", function() {      
-   	if (nav.hasClass('mobile')) nav.fadeOut('fast');      
-  	});
-
-
-  	/*----------------------------------------------------*/
-  	/* Smooth Scrolling
-  	------------------------------------------------------ */
-  	$('.smoothscroll').on('click', function (e) {
-	 	
-	 	e.preventDefault();
-
-   	var target = this.hash,
-    	$target = $(target);
-
-    	$('html, body').stop().animate({
-       	'scrollTop': $target.offset().top
-      }, 800, 'swing', function () {
-      	window.location.hash = target;
-      });
-
-  	});  
-  
-
-   /*----------------------------------------------------*/
-	/*	Modal Popup
-	------------------------------------------------------*/
-    $('.item-wrap a').magnificPopup({
-
-       type:'inline',
-       fixedContentPos: false,
-       removalDelay: 300,
-       showCloseBtn: false,
-       mainClass: 'mfp-fade'
-
-    });
-
-    $(document).on('click', '.popup-modal-dismiss', function (e) {
-    		e.preventDefault();
-    		$.magnificPopup.close();
-    });
-
-
-   /*----------------------------------------------------*/
-	/*  Placeholder Plugin Settings
-	------------------------------------------------------ */  	 
-	$('input, textarea').placeholder()  
-
-   
-	/*----------------------------------------------------*/
-	/*	contact form
-	------------------------------------------------------*/
-
-	/* local validation */
-	$('#contactForm').validate({
-
-		/* submit via ajax */
-		submitHandler: function(form) {
-
-			var sLoader = $('#submit-loader');
-
-			$.ajax({      	
-
-		      type: "POST",
-		      url: "inc/sendEmail.php",
-		      data: $(form).serialize(),
-		      beforeSend: function() { 
-
-		      	sLoader.fadeIn(); 
-
-		      },
-		      success: function(msg) {
-
-	            // Message was sent
-	            if (msg == 'OK') {
-	            	sLoader.fadeOut(); 
-	               $('#message-warning').hide();
-	               $('#contactForm').fadeOut();
-	               $('#message-success').fadeIn();   
-	            }
-	            // There was an error
-	            else {
-	            	sLoader.fadeOut(); 
-	               $('#message-warning').html(msg);
-		            $('#message-warning').fadeIn();
-	            }
-
-		      },
-		      error: function() {
-
-		      	sLoader.fadeOut(); 
-		      	$('#message-warning').html("Something went wrong. Please try again.");
-		         $('#message-warning').fadeIn();
-
-		      }
-
-	      });     		
-  		}
-
-	});
-	
+	// Call all functions
+	loader();
+	responsive();
+	heroSection();
+	testimonial();
+	progressbar();
+	videoPopup();
+	accordions();
+	progressCircle();
 
 })(jQuery);
